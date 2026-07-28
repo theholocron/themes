7e0b35ee3f564d5b88e1144f67fe8c7c693b553d
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-28 14:36:24 -0700

    chore: convert stylelint config to TypeScript
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- stylelint.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://stylelint.io/user-guide/configure/
- * @type {import("stylelint").Config}
- */
-const config = {
-	extends: ["@theholocron/stylelint-config"],
-};
-
-export default config;
