2c9d96a40721307e2d20ede4654b43c9a6f54126
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-13 09:28:25 -0700

    fix(lint): sort imports in docs/astro.config.ts
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,6 @@
 import starlight from "@astrojs/starlight";
-import { docsTheme } from "@theholocron/docs-theme";
 import { defineConfig } from "@theholocron/astro-config";
+import { docsTheme } from "@theholocron/docs-theme";
 import themesConfig from "@theholocron/themes-docs";
 
 export default defineConfig({
