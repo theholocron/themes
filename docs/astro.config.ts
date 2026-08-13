1fdb582ef52f4d55d44ec8f696d5f381a6a753f8
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-13 16:09:20 -0700

    chore(docs): adopt new @theholocron/astro-config defineConfig
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,26 +1,19 @@
 import starlight from "@astrojs/starlight";
+import { defineConfig } from "@theholocron/astro-config";
 import { docsTheme } from "@theholocron/docs-theme";
-import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "Themes",
-			plugins: [docsTheme()],
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-					href: "https://github.com/theholocron/themes",
-				},
-			],
-			sidebar: [
-				{ label: "Overview", slug: "" },
-				{
-					label: "Packages",
-					items: [{ label: "docs-theme", slug: "docs-theme" }],
-				},
-			],
-		}),
-	],
+	docs: {
+		name: "Themes",
+		github: "themes",
+		sidebar: [
+			{ label: "Overview", slug: "" },
+			{
+				label: "Packages",
+				items: [{ label: "docs-theme", slug: "docs-theme" }],
+			},
+		],
+	},
+	starlight,
+	docsTheme,
 });
