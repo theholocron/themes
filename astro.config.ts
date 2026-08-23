a1ef8e72db550e996dd1dc836d7ac95c6e72d7b1
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-23 15:03:21 -0700

    feat(docs): ✨ 📚️ add starlight-typedoc API reference generation (#64)

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "@theholocron/astro-config";
 import { docsTheme } from "@theholocron/docs-theme";
+import starlightTypeDoc from "starlight-typedoc";
 
 export default defineConfig({
 	docs: {
@@ -14,7 +15,18 @@ export default defineConfig({
 			},
 		],
 	},
-	starlight,
+	starlight: ((opts) =>
+		starlight({
+			...opts,
+			plugins: [
+				...(opts.plugins ?? []),
+				starlightTypeDoc({
+					entryPoints: ["./packages/docs-theme/src/index.ts"],
+					tsconfig: "./packages/docs-theme/tsconfig.json",
+					sidebar: { label: "API Reference", collapsed: true },
+				}),
+			],
+		})) as typeof starlight,
 	docsTheme,
 	srcDir: "./docs/src",
 	outDir: "./docs/dist",
